c5de9aa54ab8449725f526b6bb72cef22932f6fb
cley9 <cleyutp@gmail.com>
2023-12-03 10:04:00 -0500

    v1.77.0 solución the problem of vue, multiples proyect + puerto asignado for vite in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,47 +17,52 @@ import tailwindcss from 'tailwindcss'
 
 
 export default defineConfig({
-    base: '/public/',
-    plugins: [
-        laravel( 
-        //     [
-        //     'resources/css/app.css',
-        //     'resources/js/app.js',
-        //     // 'resources/js/msj.js',
-        //     // '/public/js/msj.js',
-        // ]
-        {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }
-        ),
-        // react(),
-        // viteSingleFile(),
-        // autoprefixer(),
-        vue(),
-        tailwindcss(),
-        // autoprefixer(),
-        // WindiCSS(),
-        // vue({
-        //     template: {
-        //         transformAssetUrls: {
-        //             base: null,
-        //             includeAbsolute: false,
-        //         },
-        //     },
-        // }),
-    ],
-    // this is adding for las rutas relativa, el cual sera el alias dentro del resolve
-    resolve: {
-        alias: {
-          vue: 'vue/dist/vue.esm-bundler.js',
-          "@promo":fileURLToPath(new URL("../../public/", import.meta.url)),
-        //   "@promo":fileURLToPath(new URL("/storage/img/SlayderPromocion/", import.meta.url)),
-        //   "@": fileURLToPath(new URL("../src/", import.meta.url)),
-          "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-          // puedes agregar otros alias aquí
-        },
-      },
+  // solución multiples proyecto de vue, configurando el puerto de vite 
+  server: {
+    port: 5176, // Cambia este número al puerto que prefieras
+  },
+  // 
+  base: '/public/',
+  plugins: [
+    laravel(
+      //     [
+      //     'resources/css/app.css',
+      //     'resources/js/app.js',
+      //     // 'resources/js/msj.js',
+      //     // '/public/js/msj.js',
+      // ]
+      {
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }
+    ),
+    // react(),
+    // viteSingleFile(),
+    // autoprefixer(),
+    vue(),
+    tailwindcss(),
+    // autoprefixer(),
+    // WindiCSS(),
+    // vue({
+    //     template: {
+    //         transformAssetUrls: {
+    //             base: null,
+    //             includeAbsolute: false,
+    //         },
+    //     },
+    // }),
+  ],
+  // this is adding for las rutas relativa, el cual sera el alias dentro del resolve
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+      "@promo": fileURLToPath(new URL("../../public/", import.meta.url)),
+      //   "@promo":fileURLToPath(new URL("/storage/img/SlayderPromocion/", import.meta.url)),
+      //   "@": fileURLToPath(new URL("../src/", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      // puedes agregar otros alias aquí
+    },
+  },
 });
 
 
